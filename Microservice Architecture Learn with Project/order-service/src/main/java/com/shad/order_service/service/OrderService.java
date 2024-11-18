4584b45aa725b0ff7e63f81f2320d7e56b7d6d00
Shad-Alam <shad.19bd@gmail.com>
2024-11-18 12:45:27 +0600

    adding product-service+mongodb and order-service+mysql

---
+++ Microservice Architecture Learn with Project/order-service/src/main/java/com/shad/order_service/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.shad.order_service.service;
+
+import com.shad.order_service.dto.OrderLineItemsDto;
+import com.shad.order_service.dto.OrderRequest;
+import com.shad.order_service.model.Order;
+import com.shad.order_service.model.OrderLineItems;
+import com.shad.order_service.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItemsList = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItemsList);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
--- Microservice Architecture Learn with Project/product-service/src/test/java/com/shad/product_service/ProductServiceApplicationTests.java
+++ Microservice Architecture Learn with Project/product-service/src/test/java/com/shad/product_service/ProductServiceApplicationTests.java
@@ -1,6 +1,9 @@
 package com.shad.product_service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shad.product_service.dto.ProductRequest;
+import com.shad.product_service.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,8 +16,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 
@@ -34,8 +35,11 @@ class ProductServiceApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
 	}
 
@@ -45,11 +49,12 @@ class ProductServiceApplicationTests {
 		String productRequestString = objectMapper.writeValueAsString(productRequest);
 
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(productRequestString))
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(productRequestString))
 				.andExpect(status().isCreated());
-		//);
 
+
+		Assertions.assertTrue(productRepository.findAll().size()==1);
 	}
 
 	private ProductRequest getProductRequest() {
@@ -59,5 +64,4 @@ class ProductServiceApplicationTests {
 				.price(BigDecimal.valueOf(1230))
 				.build();
 	}
-
 }
